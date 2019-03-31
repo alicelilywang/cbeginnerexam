e5605dd4af693278764d1724d2356a399ce743f5
Alice Wang <alice.y.wang15@gmail.com>
2019-03-31 13:04:47 -0700

    Update level02.c

--- level02.c
+++ level02.c
@@ -1,9 +1,14 @@
 /********************************* 1/15 ispoweroftwo.c ****************************** */
+Write a function that determines if a given number is a power of 2.
+This function returns 1 if the given number is a power of 2, otherwise it returns 0.
 int	    is_power_of_2(unsigned int n)
 {
 	return ((n & (-n)) == n ? 1 : 0);
 }
 /********************************* 2/15 alphamirror.c ****************************** */
+Write a program called alpha_mirror that takes a string and displays this string
+after replacing each alphabetical character by the opposite alphabetical
+character, followed by a newline.
 #include <unistd.h>
 int		main(int argc, char *argv[])
 {
@@ -27,6 +32,12 @@ int		main(int argc, char *argv[])
 	return (0);
 }
 /********************************* 3/15 wdmatch.c ****************************** */
+Write a program that takes two strings and checks whether it's possible to
+write the first string with characters from the second string, while respecting
+the order in which these characters appear in the second string.
+If it's possible, the program displays the string, followed by a \n, otherwise
+it simply displays a \n.
+If the number of arguments is not 2, the program displays a \n.
 #include <unistd.h>
 void	ft_putstr(char const *str)
 {
@@ -55,6 +66,8 @@ int		main(int argc, char const *argv[])
 	return (0);
 }
 /********************************* 4/15 ft_atoi.c ****************************** */
+Write a function that converts the string argument str to an integer (type int)
+and returns it.
 int		ft_atoi(char *str)
 {
 	int		i;
@@ -77,6 +90,16 @@ int		ft_atoi(char *str)
 	return (nbr * sign);
 }
 /********************************* 5/15 do_op.c ****************************** */
+Write a program that takes three strings:
+- The first and the third one are representations of base-10 signed integers
+  that fit in an int.
+- The second one is an arithmetic operator chosen from: + - * / %
+The program must display the result of the requested arithmetic operation,
+followed by a newline. If the number of parameters is not 3, the program
+just displays a newline.
+You can assume the string have no mistakes or extraneous characters. Negative
+numbers, in input or output, will have one and only one leading '-'. The
+result of the operation fits in an int.
 #include <stdio.h>
 #include <stdlib.h>
 int		main(int argc, char *argv[])
@@ -98,6 +121,9 @@ int		main(int argc, char *argv[])
 	return (0);
 }
 /********************************* 6/15 ft_strcmp.c ****************************** */
+Compares the C string str1 to the C string str2.
+This function starts comparing the first character of each string. If they are equal to each other, it continues with the following pairs until the characters differ or until a terminating null-character is reached.
+This function performs a binary comparison of the characters. 
 int		ft_strcmp(char *s1, char *s2)
 {
 	while (*s1 && (*s1 == *s2))
@@ -108,6 +134,9 @@ int		ft_strcmp(char *s1, char *s2)
 	return (*s1 - *s2);
 }
 /********************************* 7/15 ft_strdup.c ****************************** */
+The strdup() function returns a pointer to a new string which is a
+duplicate of the string s.  Memory for the new string is obtained
+with malloc(3), and can be freed with free(3).
 #include <stdlib.h>
 
 int		ft_strlen(char *str)
@@ -133,6 +162,8 @@ char	*ft_strdup(char *src)
 	return (cpy);
 }
 /********************************* 8/15 ft_strrev.c ****************************** */
+Write a function that reverses (in-place) a string.
+It must return its parameter.
 char		*ft_strrev(char *str)
 {
 	int		i;
@@ -153,6 +184,10 @@ char		*ft_strrev(char *str)
 	return (str);
 }
 /********************************* 9/15 union.c ****************************** */
+Write a program that takes two strings and displays, without doubles, the
+characters that appear in either one of the strings.
+The display will be in the order characters appear in the command line, and
+will be followed by a \n. If the number of arguments is not 2, the program displays \n.
 #include <unistd.h>
 void	ft_union(char *str1, char *str2)
 {
@@ -188,6 +223,9 @@ int		main (int argc, char *argv[])
 	return (0);
 }
 /********************************* 10/15 inter.c ****************************** */
+Write a program that takes two strings and displays, without doubles, the
+characters that appear in both strings, in the order they appear in the first
+one. The display will be followed by a \n. If the number of arguments is not 2, the program displays \n.
 #include <unistd.h>
 int     iter(char *str, char c, int len)
 {
@@ -217,6 +255,9 @@ int     main(int argc, char *argv[])
     return (0);
 }
 /********************************* 11/15 last_word.c ****************************** */
+Write a program that takes a string and displays its last word followed by a \n.
+A word is a section of string delimited by spaces/tabs or by the start/end of
+the string. If the number of parameters is not 1, or there are no words, display a newline.
 #include <unistd.h>
 void	fn_word(char *str, int *i)
 {
@@ -260,6 +301,11 @@ int		main(int argc, char **argv)
 	return (0);
 }
 /********************************* 12/15 max.c ****************************** */
+Write the following function:
+int		max(int* tab, unsigned int len);
+The first parameter is an array of int, the second is the number of elements in
+the array. The function returns the largest number found in the array. 
+If the array is empty, the function returns 0.
 int		max(int *tab, unsigned int len)
 {
 	int		i;
@@ -277,6 +323,10 @@ int		max(int *tab, unsigned int len)
 	return (max_value);
 }
 /********************************* 13/15 print_bits.c ****************************** */
+Write a function that takes a byte, and prints it in binary WITHOUT A NEWLINE
+AT THE END.Your function must be declared as follows:
+void	print_bits(unsigned char octet);
+Example, if you pass 2 to print_bits, it will print "00000010".
 #include <unistd.h>
 void	print_bits(unsigned char octet)
 {
@@ -291,6 +341,9 @@ void	print_bits(unsigned char octet)
 	}
 }
 /********************************* 14/15 reverse_bits.c ****************************** */
+Write a function that takes a byte, reverses it, bit by bit (like the
+example) and returns the result. Your function must be declared as follows:
+unsigned char	reverse_bits(unsigned char octet); 
 unsigned char   reverse_bits(unsigned char octet)
 {
 	return  (((octet >> 0) & 1) << 7) | \
@@ -303,6 +356,10 @@ unsigned char   reverse_bits(unsigned char octet)
 			(((octet >> 7) & 1) << 0);
 }
 /********************************* 15/15 swap_bits.c ****************************** */
+Write a function that takes a byte, swaps its halves (like the example) and
+returns the result. Your function must be declared as follows:
+
+unsigned char	swap_bits(unsigned char octet);
 unsigned char	swap_bits(unsigned char c)
 {
 	return ((c >> 4) | (c << 4));
